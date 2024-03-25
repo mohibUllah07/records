9409399ccbde88b93558ff578ec0d867e0f343ec
mohibUllah07 <122685562+mohibUllah07@users.noreply.github.com>
2024-03-25 16:28:53 +0500

    Add files via upload

---
+++ records/Program.cs
@@ -0,0 +1,72 @@
+﻿
+
+List<object> Allpersons = new List<object>()
+{
+new PERSON()
+{
+    name = "mohibullah",
+    lastName = "khan",
+    age = 20
+},
+
+new PERSON()
+{
+    name = "dsdfas",
+    lastName = "khdsfgan",
+    age = 20
+},
+
+
+new PERSON()
+{
+    name = "dsf",
+    lastName = "s",
+    age = 20
+},
+
+
+new PERSON()
+{
+    name = "uskhdfkls",
+    lastName = "ksbjkf",
+    age = 20
+}
+
+
+};
+
+/*PERSON p2 = new()
+{
+    name = "azeen",
+    lastName = "shazan",
+    age = 29
+};
+
+PERSON p3 = new()
+{
+    name = "kami",
+    lastName = "khan",
+    age = 10
+};
+
+PERSON p4 = new()
+{
+    name = "kashif",
+    lastName = "khan",
+    age = 30
+};*/
+//Console.WriteLine(p1);
+
+foreach (PERSON n in Allpersons)
+
+{
+    Console.WriteLine(n);
+}
+
+
+public record PERSON()
+{
+    public string name;
+    public string lastName;
+    public int age;
+}
